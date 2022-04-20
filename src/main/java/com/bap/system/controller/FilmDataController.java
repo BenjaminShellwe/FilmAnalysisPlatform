19f013a98a4d9c1df7809d12fe000a91b159ba1d
BenjaminThomas <benjaminshellwe@outlook.com>
2022-04-21 03:11:52 +0800

    bap-admin 2022/04/21 deadline

--- src/main/java/com/bap/system/controller/FilmDataController.java
+++ src/main/java/com/bap/system/controller/FilmDataController.java
@@ -5,21 +5,25 @@ package com.bap.system.controller;
  * Date 2022/4/19 10:5:55
  */
 
-import com.bap.system.mapper.BasicUserMapper;
+import com.alibaba.fastjson.JSONObject;
 import com.bap.system.mapper.FilmMapper;
 import com.bap.util.Result;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
+import java.util.Map;
 
 @Controller
 @RequestMapping("/film")
 public class FilmDataController {
 
+//    基本mapper调用请看下面函数的使用,为了方便理解,都是一个接口对应一次调用
     @Autowired
     FilmMapper filmMapper;
-    BasicUserMapper basicUserMapper;
 
     @ResponseBody
     @RequestMapping(value = "/count")
@@ -28,4 +32,26 @@ public class FilmDataController {
         return new Result(s);
     }
 
+    @ResponseBody
+    @RequestMapping(value = "/yearsCount")
+    public Result yearsCount() {
+        List<Map<String, Integer>> s = filmMapper.yearsCount();
+        return new Result(s);
+    }
+
+    @ResponseBody
+    @RequestMapping(value = "/randomFilm")
+    public Result randomFilm() {
+        List<Map<String, Integer>> s = filmMapper.randomFilm();
+        return new Result(s);
+    }
+
+    @ResponseBody
+    @RequestMapping(value = "/budgetFilm")
+    public Result budgetFilm(@RequestBody JSONObject jsonObject) {
+        List<Map<String, Integer>> s = filmMapper.budgetFilm(jsonObject.getInteger("i"),jsonObject.getInteger("j"));
+        System.out.println("测试输出" + jsonObject);
+        System.out.println(s);
+        return new Result(s);
+    }
 }
--- src/main/java/com/bap/system/mapper/FilmMapper.java
+++ src/main/java/com/bap/system/mapper/FilmMapper.java
@@ -12,10 +12,28 @@ import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Map;
+
 @Mapper
 @Repository
 public interface FilmMapper extends BaseMapper<FilmData> {
 
     @Select("select count(*) from data1")
     String count();
+    @Select("SELECT `release`, COUNT(*) AS total " +
+            "FROM data1 " +
+            "GROUP BY data1.`release` " +
+            "HAVING COUNT(1) >= 1 " +
+            "ORDER BY `release` ASC")
+    List<Map<String, Integer>> yearsCount();
+
+    @Select("SELECT score, f1, title, director FROM data1 ORDER BY RAND() LIMIT 1")
+    List<Map<String, Integer>> randomFilm();
+
+    @Select("SELECT data1.budget, data1.boxoffice, data1.title " +
+            "FROM data1 " +
+            "WHERE data1.f1 BETWEEN #{i} AND #{j} " +
+            "GROUP BY  data1.f1")
+    List<Map<String, Integer>> budgetFilm(int i, int j);
 }
